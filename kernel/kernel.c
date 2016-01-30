b01be9f11d8052a326b951804fa289b5bec05210
Hongyuan Wang <710136012@qq.com>
2016-01-31 01:37:15 +0800

    print hello word

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -32,35 +32,36 @@ cmain (unsigned long magic, unsigned long addr)
   
   /* Clear the screen.  */
   cls ();
-
+  printf("hello world");
+  
   /* Am I booted by a Multiboot-compliant boot loader?  */
-  if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
+  /*if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
     {
       printf ("Invalid magic number: 0x%x\n", (unsigned) magic);
       return;
     }
-
+  */
   /* Set MBI to the address of the Multiboot information structure.  */
-  mbi = (multiboot_info_t *) addr;
+  //mbi = (multiboot_info_t *) addr;
 
   /* Print out the flags.  */
-  printf ("flags = 0x%x\n", (unsigned) mbi->flags);
+  //printf ("flags = 0x%x\n", (unsigned) mbi->flags);
 
   /* Are mem_* valid?  */
-  if (CHECK_FLAG (mbi->flags, 0))
+  /*if (CHECK_FLAG (mbi->flags, 0))
     printf ("mem_lower = %uKB, mem_upper = %uKB\n",
 	    (unsigned) mbi->mem_lower, (unsigned) mbi->mem_upper);
-
+  */
   /* Is boot_device valid?  */
-  if (CHECK_FLAG (mbi->flags, 1))
-    printf ("boot_device = 0x%x\n", (unsigned) mbi->boot_device);
+  //if (CHECK_FLAG (mbi->flags, 1))
+  //  printf ("boot_device = 0x%x\n", (unsigned) mbi->boot_device);
   
   /* Is the command line passed?  */
-  if (CHECK_FLAG (mbi->flags, 2))
+  /*if (CHECK_FLAG (mbi->flags, 2))
     printf ("cmdline = %s\n", (char *) mbi->cmdline);
-
+  */
   /* Are mods_* valid?  */
-  if (CHECK_FLAG (mbi->flags, 3))
+  /*if (CHECK_FLAG (mbi->flags, 3))
     {
       module_t *mod;
       int i;
@@ -75,16 +76,16 @@ cmain (unsigned long magic, unsigned long addr)
 		(unsigned) mod->mod_end,
 		(char *) mod->string);
     }
-
+  */
   /* Bits 4 and 5 are mutually exclusive!  */
-  if (CHECK_FLAG (mbi->flags, 4) && CHECK_FLAG (mbi->flags, 5))
+  /*if (CHECK_FLAG (mbi->flags, 4) && CHECK_FLAG (mbi->flags, 5))
     {
       printf ("Both bits 4 and 5 are set.\n");
       return;
-    }
+    }*/
 
   /* Is the symbol table of a.out valid?  */
-  if (CHECK_FLAG (mbi->flags, 4))
+  /*if (CHECK_FLAG (mbi->flags, 4))
     {
       aout_symbol_table_t *aout_sym = &(mbi->u.aout_sym);
       
@@ -93,10 +94,10 @@ cmain (unsigned long magic, unsigned long addr)
 	      (unsigned) aout_sym->tabsize,
 	      (unsigned) aout_sym->strsize,
 	      (unsigned) aout_sym->addr);
-    }
+    }*/
 
   /* Is the section header table of ELF valid?  */
-  if (CHECK_FLAG (mbi->flags, 5))
+  /*if (CHECK_FLAG (mbi->flags, 5))
     {
       elf_section_header_table_t *elf_sec = &(mbi->u.elf_sec);
 
@@ -104,10 +105,10 @@ cmain (unsigned long magic, unsigned long addr)
 	      " addr = 0x%x, shndx = 0x%x\n",
 	      (unsigned) elf_sec->num, (unsigned) elf_sec->size,
 	      (unsigned) elf_sec->addr, (unsigned) elf_sec->shndx);
-    }
+    }*/
 
   /* Are mmap_* valid?  */
-  if (CHECK_FLAG (mbi->flags, 6))
+  /*if (CHECK_FLAG (mbi->flags, 6))
     {
       memory_map_t *mmap;
       
@@ -125,6 +126,6 @@ cmain (unsigned long magic, unsigned long addr)
 		(unsigned) mmap->length_high,
 		(unsigned) mmap->length_low,
 		(unsigned) mmap->type);
-    }
+    }*/
 }    
 
